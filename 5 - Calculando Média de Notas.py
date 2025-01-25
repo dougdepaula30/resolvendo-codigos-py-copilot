aef26a27015c0f4058327afb1b2bb695b1b09a90
Douglas Gonçalves de Paula <78982397+dougdepaula30@users.noreply.github.com>
2025-01-25 19:00:52 -0300

    atualização 2.0

---
+++ 5 - Calculando Média de Notas.py
@@ -0,0 +1,12 @@
+# Agora vamos calcular a média de três notas fornecidas na entrada do usuário. Uma dica é: Utilize operadores aritméticos para realizar o cálculo da média.
+
+# Solicita ao usuário que insira as três notas
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+# Calcula a média das três notas
+media = (nota1 + nota2 + nota3) / 3
+
+# Exibe o resultado
+print("A média das três notas é:", media)
---
+++ resolucoes_code/3 - Operações Matemáticas Simples.py
@@ -0,0 +1,13 @@
+# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+# Solicita ao usuário que insira o primeiro número
+numero1 = float(input("Digite o primeiro número: "))
+
+# Solicita ao usuário que insira o segundo número
+numero2 = float(input("Digite o segundo número: "))
+
+# Realiza uma operação simples (adição) entre os dois números
+resultado = numero1 + numero2
+
+# Exibe o resultado
+print("O resultado da adição é:", resultado)
